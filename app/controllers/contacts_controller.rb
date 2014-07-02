ede0a7cccc0b980ce69f4995622b62ea72c045a7
Zack Siri <zack@codemy.net>
2014-07-02 17:28:04 +0700

    contacts crud complete

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -14,6 +14,24 @@ class ContactsController < ApplicationController
     end
   end
 
+  def edit
+    @contact = Contact.where(id: params[:id]).first
+  end
+
+  def update
+    @contact = Contact.where(id: params[:id]).first
+    if @contact.update_attributes(contact_params)
+      redirect_to contacts_path
+    end
+  end
+
+  def destroy
+    @contact = Contact.where(id: params[:id]).first
+    if @contact.destroy
+      redirect_to contacts_path
+    end
+  end
+
 private
   def contact_params
     params.require(:contact).permit(:name, :email, :address)
--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ContactsController, :type => :controller do
+  fixtures :contacts
+
+  let(:contact) { contacts(:contact_2) }
+
   describe "GET index" do 
     it "should be success" do
       get :index
@@ -22,4 +26,25 @@ RSpec.describe ContactsController, :type => :controller do
       expect(response.status).to eq(302)
     end
   end
+
+  describe "GET edit" do 
+    it "should be success" do 
+      get :edit, id: contact.id
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "PATCH/PUT update" do 
+    it "should be redirect" do 
+      patch :update, id: contact.id, contact: { name: "Steve Jobs" }
+      expect(response.status).to eq(302)
+    end
+  end 
+
+  describe "DELETE destroy" do 
+    it "should be redirect" do 
+      delete :destroy, id: contact.id
+      expect(response.status).to eq(302)
+    end
+  end 
 end
