eef32815f493f73361e0cbbca44744cf2b628d0b
Zack Siri <zack@codemy.net>
2014-07-04 01:55:27 +0700

    cleaned up layout for settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,12 +1,17 @@
 class SettingsController < ApplicationController
-	layout 'single_card'
+  before_filter :load_configurables
 
   def index
     redirect_to setting_path(Configurable.first.slug)
   end
 	
   def show
-    @configurables = Configurable.all
     @configurable = Configurable.friendly.find(params[:id])
+  end
+  
+private
+
+  def load_configurables
+    @configurables = Configurable.all
   end
 end
