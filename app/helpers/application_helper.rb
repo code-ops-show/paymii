cc43111504cdcc645b25b7c37ed7fa887bd2143f
Zack Siri <zack@codemy.net>
2014-07-06 18:43:20 +0700

    link active for edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,12 @@ module ApplicationHelper
   	end
   end
 
+  def nav_fuzzy_link name, path='#'
+    content_tag :li, class: nav_fuzzy_active?(path) do 
+      link_to name, path
+    end
+  end
+
   def link_with_icon name, path, icon_name, options={}
   	link_to path, options do 
   		icon(icon_name) + " #{name}"
@@ -13,6 +19,10 @@ module ApplicationHelper
 
   def nav_active? path
     request.path == path ? 'active' : nil
+  end
+
+  def nav_fuzzy_active? path
+    request.path.include?(path) ? 'active' : nil
   end
 
   def display label, value
