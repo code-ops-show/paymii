fa3f0c13a51ece7425b5091ace7d9c6d9701efb1
Zack Siri <zack@codemy.net>
2014-07-07 01:50:36 +0700

    can now update settings

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -12,14 +12,25 @@ class SettingsController < ApplicationController
   def edit
     @configurable = set_configurable
   end
+
+  def update
+    @configurable = set_configurable
+    if @configurable.update_attributes(configurable_params)
+      redirect_to setting_path(@configurable)
+    end
+  end
   
 private
+
+  def configurable_params
+    params.require(:configurable).permit!
+  end
 
   def set_configurable
     Configurable.friendly.find(params[:id])
   end
 
   def load_configurables
-    @configurables = Configurable.all
+    @configurables = Configurable.all.order(:name)
   end
 end
--- app/models/configurable.rb
+++ app/models/configurable.rb
@@ -2,4 +2,6 @@ class Configurable < ActiveRecord::Base
   extend FriendlyId
 
   friendly_id :name, use: :slugged
+
+  store_accessor :settings, :time_zone, :base_currency, :date_format, :currency_display
 end
