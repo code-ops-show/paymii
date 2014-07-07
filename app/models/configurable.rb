3bfef269c27aed8d7cccf57ac2e4f9caae3314ec
Jim Skogman <yim.apichai@gmail.com>
2014-07-07 19:27:04 +0700

    Added form fields for company and invoice partials

--- app/models/configurable.rb
+++ app/models/configurable.rb
@@ -3,5 +3,6 @@ class Configurable < ActiveRecord::Base
 
   friendly_id :name, use: :slugged
 
-  store_accessor :settings, :time_zone, :base_currency, :date_format, :currency_display
+  store_accessor :settings, :time_zone, :base_currency, :date_format, :currency_display, 
+  							 :company_name, :address, :logo, :title
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :contacts
-
+  
   resources :settings
 
   root to: "contacts#index"
