7b5815f94835e78c04273c0352cb06aeaa169e58
Zack Siri <zack@codemy.net>
2014-07-07 12:52:00 +0700

    fix navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,14 @@
 module ApplicationHelper
   def nav_link name, path='#'
-  	content_tag :li, class: nav_active?(path) do 
+  	content_tag :li, class: nav_fuzzy_active?(path) do 
   		link_to name, path
   	end
-  end
-
-  def nav_fuzzy_link name, path='#'
-    content_tag :li, class: nav_fuzzy_active?(path) do 
-      link_to name, path
-    end
   end
 
   def link_with_icon name, path, icon_name, options={}
   	link_to path, options do 
   		icon(icon_name) + " #{name}"
   	end
-  end
-
-  def nav_active? path
-    request.path == path ? 'active' : nil
   end
 
   def nav_fuzzy_active? path
